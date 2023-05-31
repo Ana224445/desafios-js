1474baf2f0bbca25ff7aea930ecc39c4cab3412e
Farroupilhas <106492639+Ana224445@users.noreply.github.com>
2023-05-30 22:35:00 -0300

    Add files via upload

---
+++ desafios_karol/mensagens.js
@@ -0,0 +1,13 @@
+let resp = window.document.getElementById('saida')
+
+function botao1() {
+    window.alert('Você clicou no 1° botão!')
+}
+
+function botao2() {
+    window.alert('Você clicou no 2° botão!')
+}
+
+function botao3() {
+    window.alert('Você clicou no 3° botão!')
+}
